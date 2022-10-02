7ce6d8160d741d18cedcaa8d8a7cbbe3530eb73d
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2022-10-02 23:45:59 +0100

    added private navbar

--- src/Router/PrivateRoute.js
+++ src/Router/PrivateRoute.js
@@ -1,8 +1,8 @@
-import {Navigate} from 'react-router';
+import { Navigate } from "react-router";
 
-const PrivateRoute = ({children}) => {
-  const auth = window.sessionStorage.getItem('token');
-  return auth ? children : <Navigate to={'/login'}/>;
+const PrivateRoute = ({ children }) => {
+  const auth = window.sessionStorage.getItem("token");
+  return auth ? children : <Navigate to={"/login"} />;
 };
 
 export default PrivateRoute;
