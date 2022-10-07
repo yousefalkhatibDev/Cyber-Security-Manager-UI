e5561bef499f7161e85518b13dc83a81ae3bd6c3
mohammad alkhatib <mo_khatib@outlook.com>
2022-10-07 13:50:00 +0300

    fix posts dashboard and add an extra link on the panigation

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -24,6 +24,7 @@ export default class Pagination extends Component {
 
                     })
                 }
+                <a href="#">...</a>
                 <a href="#">»</a>
             </div>
         )
