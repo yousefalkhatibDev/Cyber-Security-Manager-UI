ee37e60722ee18d3dad3cb3b7e54794766265823
ahmad almuhidat <ahmad.almuhidat@gmail.com>
2022-10-14 00:18:07 +0100

    added dashboard routes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import OperationCard from "../components/OperationCard";
 import TargetCard from "../components/TargetCard";
 import API from "../helper/API";
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import ProgressBar from "react-bootstrap/ProgressBar";
 // import Summary from "../components/Summary";
 
 class Dashboard extends React.Component {
@@ -13,20 +13,57 @@ class Dashboard extends React.Component {
       recentPosts: [],
       targets: [],
       recentTargets: [],
-      progress: 20
+      progress: 20,
+      TargetsCount: 0,
+      OperationsCount: 0,
     };
+
     this.GetOperations = this.GetOperations.bind(this);
-    this.GetRecentPosts = this.GetRecentPosts.bind(this)
+    this.GetRecentPosts = this.GetRecentPosts.bind(this);
     this.GetPosts = this.GetPosts.bind(this);
     this.GetTargets = this.GetTargets.bind(this);
     this.GetRecentTargets = this.GetRecentTargets.bind(this);
+    this.GetTargetsCount = this.GetTargetsCount.bind(this);
+    this.GetOperationsCount = this.GetOperationsCount.bind(this);
+  }
+
+  async GetTargetsCount() {
+    const token = window.sessionStorage.getItem("token");
+    const data = { Token: token };
+
+    await API.post("/get_targets_count", data)
+      .then((respone) => {
+        const res = respone.data;
+        if (res.data) {
+          this.setState({ TargetsCount: res.data[0].TargetsCount });
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  async GetOperationsCount() {
+    const token = window.sessionStorage.getItem("token");
+    const data = { Token: token };
+
+    await API.post("/get_operations_count", data)
+      .then((respone) => {
+        const res = respone.data;
+        if (res.data) {
+          this.setState({ OperationsCount: res.data[0].OperationsCount });
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   }
 
   async GetOperations() {
     const token = window.sessionStorage.getItem("token");
     const data = { Token: token, search: this.state.search };
 
-    await API.post("/get_operations", data)
+    await API.post("/get_last_accessed_operation", data)
       .then((respone) => {
         const res = respone.data;
         if (res.data) {
@@ -39,14 +76,15 @@ class Dashboard extends React.Component {
   }
 
   async GetPosts(id) {
-    const data = {
-      OperationID: id
-    };
+    const data = { OperationID: id };
+
     await API.post("/get_posts", data)
       .then((respone) => {
         const res = respone.data;
         if (res.data) {
-          this.setState({ recentPosts: [...this.state.recentPosts, ...res.data] });
+          this.setState({
+            recentPosts: [...this.state.recentPosts, ...res.data],
+          });
         }
       })
       .catch(function (error) {
@@ -58,7 +96,7 @@ class Dashboard extends React.Component {
     const token = window.sessionStorage.getItem("token");
     const data = { Token: token, search: this.state.search };
 
-    await API.post("/get_targets_by_user", data)
+    await API.post("/get_last_accessed_target", data)
       .then((respone) => {
         const res = respone.data;
         if (res.data) {
@@ -73,32 +111,34 @@ class Dashboard extends React.Component {
   GetRecentTargets() {
     for (let i = 0; i < this.state.targets.length; i++) {
       if (this.state.recentTargets.length > 5) return;
-      this.setState({ recentTargets: [...this.state.recentTargets, this.state.targets[i]] })
+      this.setState({
+        recentTargets: [...this.state.recentTargets, this.state.targets[i]],
+      });
     }
   }
 
   async GetRecentPosts() {
     for (let i = 0; i <= this.state.operations.length; i++) {
-      if (this.state.operations[i] == undefined) return;
+      if (this.state.operations[i] === undefined) return;
       if (this.state.recentPosts.length > 5) return;
       // this.setState({ recentOperations: [...this.state.recentOperations, this.state.operations[i]] });
-      const posts = await this.GetPosts(this.state.operations[i].o_id)
-    };
+      const posts = await this.GetPosts(this.state.operations[i].o_id);
+    }
   }
 
   GoToPostOperation(id) {
-    window.location.href = `/operation_profile/${id}`
+    window.location.href = `/operation_profile/${id}`;
   }
 
   GoToTarget(id) {
-    window.location.href = `/target_profile/${id}`
+    window.location.href = `/target_profile/${id}`;
   }
 
   async componentDidMount() {
     await this.GetOperations();
     await this.GetTargets();
     this.GetRecentPosts();
-    this.GetRecentTargets()
+    this.GetRecentTargets();
   }
 
   render() {
@@ -107,9 +147,7 @@ class Dashboard extends React.Component {
         <div className="userRecentAccess-Container">
           <div className="userOperations-recent">
             <h4>Last accessed operation</h4>
-            {
-              this.state.operations.length
-              &&
+            {this.state.operations.length && (
               <OperationCard
                 key={1}
                 id={this.state.operations[0].o_id}
@@ -119,13 +157,11 @@ class Dashboard extends React.Component {
                 CreateDate={this.state.operations[0].o_create_date}
                 width={90}
               />
-            }
+            )}
           </div>
           <div className="userTargets-recent">
             <h4>Last accessed target</h4>
-            {
-              this.state.targets.length
-              &&
+            {this.state.targets.length && (
               <TargetCard
                 key={2}
                 id={this.state.targets[0].t_id}
@@ -137,57 +173,71 @@ class Dashboard extends React.Component {
                 UpdateDate={this.state.targets[0].t_update_date}
                 width={90}
               />
-            }
+            )}
           </div>
         </div>
 
         <div className="userInfoCards-Container">
           <div className="userInfoCards-card userInfoCards-recentPosts">
             <h5>Recent Posts</h5>
-            {
-              this.state.recentPosts.map((el) => {
-                return (
-                  <div className="postCard" key={el.p_id}>
-                    <div className="postCard-content">
-                      <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" />
-                      <div className="postCard-text">
-                        <h5>{el.p_title}</h5>
-                        <h6>By {el.u_name}</h6>
-                        <p>{el.p_text}</p>
-                      </div>
+            {this.state.recentPosts.map((el) => {
+              return (
+                <div className="postCard" key={el.p_id}>
+                  <div className="postCard-content">
+                    <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt=""/>
+                    <div className="postCard-text">
+                      <h5>{el.p_title}</h5>
+                      <h6>By {el.u_name}</h6>
+                      <p>{el.p_text}</p>
                     </div>
-                    <button class="OperationCardContainerButton" onClick={() => this.GoToPostOperation(el.p_operation)}>Navigate to Operation</button>
                   </div>
-                )
-              })
-            }
+                  <button
+                    class="OperationCardContainerButton"
+                    onClick={() => this.GoToPostOperation(el.p_operation)}
+                  >
+                    Navigate to Operation
+                  </button>
+                </div>
+              );
+            })}
           </div>
           <div className="userInfoCards-card userInfoCards-recentTargets">
             <h5>Recent Targets</h5>
-            {
-              this.state.recentTargets.map((el) => {
-                return (
-                  <div className="targetCard" key={el.t_id}>
-                    <div className="targetCard-content">
-                      <img src="https://img.freepik.com/premium-vector/anonymous-hacker-concept-with-flat-design_23-2147895788.jpg?w=740" />
-                      <div className="targetCard-text">
-                        <h5>{el.t_name}</h5>
-                        <p>{el.t_description}</p>
-                      </div>
+            {this.state.recentTargets.map((el) => {
+              return (
+                <div className="targetCard" key={el.t_id}>
+                  <div className="targetCard-content">
+                    <img
+                      src="https://img.freepik.com/premium-vector/anonymous-hacker-concept-with-flat-design_23-2147895788.jpg?w=740"
+                      alt=""
+                    />
+                    <div className="targetCard-text">
+                      <h5>{el.t_name}</h5>
+                      <p>{el.t_description}</p>
                     </div>
-                    <button class="OperationCardContainerButton" onClick={() => this.GoToTarget(el.t_id)}>Navigate to Target</button>
                   </div>
-                )
-              })
-            }
+                  <button
+                    class="OperationCardContainerButton"
+                    onClick={() => this.GoToTarget(el.t_id)}
+                  >
+                    Navigate to Target
+                  </button>
+                </div>
+              );
+            })}
           </div>
           <div className="userInfoCards-card">
             <h5>Level</h5>
             <div className="userInfoCards-NextLevel">
-              <p>Next Rank - <span>Level2</span></p>
+              <p>
+                Next Rank - <span>Level2</span>
+              </p>
             </div>
             <div className="userInfoCards-ProgressBar">
-              <ProgressBar now={this.state.progress} label={`${this.state.progress}%`} />
+              <ProgressBar
+                now={this.state.progress}
+                label={`${this.state.progress}%`}
+              />
             </div>
             <div className="userInfoCards-NextLevel">
               <p>10/200</p>
@@ -195,8 +245,12 @@ class Dashboard extends React.Component {
           </div>
           <div className="userInfoCards-card">
             <h5>More information</h5>
-            <p className="userInfoCount">Operations: <span>2</span></p>
-            <p className="userInfoCount">Targets: <span>4</span></p>
+            <p className="userInfoCount">
+              Operations: <span>{this.state.OperationsCount}</span>
+            </p>
+            <p className="userInfoCount">
+              Targets: <span>{this.state.TargetsCount}</span>
+            </p>
           </div>
         </div>
       </div>
