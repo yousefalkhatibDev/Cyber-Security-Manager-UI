3c4101f5b701d2345e45c23a22d21276086512da
Yousef Alkhatib <yousefalkhatib01@gmail.com>
2022-11-09 21:04:16 +0200

    make the sidebar responsive

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,12 +1,32 @@
+import { toHaveFocus } from "@testing-library/jest-dom/dist/matchers";
 import React, { Component } from "react";
 
 class ContentWrapper extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isSideBarClosed: false
+        }
     }
+
+    componentDidMount() {
+        if (window.innerWidth <= 920 && !this.state.isSideBarClosed) {
+            this.setState({ isSideBarClosed: true })
+        } else if (window.innerWidth > 920 && this.state.isSideBarClosed) {
+            this.setState({ isSideBarClosed: false })
+        }
+        window.addEventListener("resize", () => {
+            if (window.innerWidth <= 920 && !this.state.isSideBarClosed) {
+                this.setState({ isSideBarClosed: true })
+            } else if (window.innerWidth > 920 && this.state.isSideBarClosed) {
+                this.setState({ isSideBarClosed: false })
+            }
+        })
+    }
+
     render() {
         return (
-            <div className='ContentWrapper'>
+            <div className={!this.state.isSideBarClosed ? 'ContentWrapper' : 'ContentWrapper ContentWrapper-SideBarClosed'}>
                 {this.props.children}
             </div>
         )
